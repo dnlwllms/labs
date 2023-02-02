2bb3a019eecadd80e8d4c25d3712edba45803adb
dnlwllms <daniel.williams.kr@gmail.com>
2023-02-02 16:08:39 +0900

    add: Form Component

--- src/packages/form/Form.tsx
+++ src/packages/form/Form.tsx
@@ -1,11 +1,142 @@
-import React, { PropsWithChildren } from "react";
+import {
+  createContext,
+  FC,
+  ChangeEvent,
+  FormEvent,
+  PropsWithChildren,
+  ReactElement,
+  useState,
+  useEffect,
+  cloneElement,
+  useContext,
+} from "react";
 
-interface FormProps extends PropsWithChildren {}
+type FormValidation<T> = Array<{
+  key: keyof T;
+  regExp: RegExp;
+  message: string;
+}>;
 
-const Form: React.FC<FormProps> = ({ children }) => {
-  return <div>{children}</div>;
+type FormError<T> = Partial<Record<keyof T, string[]>>;
+
+type UseFormParams<T> = {
+  initialValues: T;
+  validation?: FormValidation<T>;
+};
+
+type UseFormReturn<T = Record<string, unknown>> = {
+  values: T;
+  errors: FormError<T>;
+  handleValue: (key: keyof T, value: unknown) => void;
+};
+
+const FormContext = createContext<UseFormReturn>({
+  values: {},
+  errors: {},
+  handleValue: console.debug,
+});
+
+export const useForm = <T extends Record<string, unknown>>({
+  initialValues,
+  validation,
+}: UseFormParams<T>): UseFormReturn<T> => {
+  const [values, setValues] = useState<T>(initialValues);
+  const [errors, setErrors] = useState<FormError<T>>({});
+
+  const [validationState] = useState(validation);
+
+  useEffect(() => {
+    if (validationState) {
+      const invalidItems = validationState.filter((item) => {
+        const instance = new RegExp(item.regExp);
+
+        return !instance.test(String(values[item.key]));
+      });
+
+      let newErrors: FormError<T> = {};
+      invalidItems.forEach(({ key, message }) => {
+        if (newErrors[key]) {
+        } else {
+          newErrors[key] = [message];
+        }
+      });
+      console.log(newErrors);
+
+      setErrors(newErrors);
+    }
+  }, [validationState, values]);
+
+  const handleValue = (key: keyof T, value: unknown) => {
+    setValues({
+      ...values,
+      [key]: value,
+    });
+  };
+
+  return {
+    values,
+    errors,
+    handleValue,
+  };
+};
+
+interface InternalForm {
+  Item: FC<ItemProps>;
+  Input: FC<InputProps>;
+}
+
+interface FormProps extends PropsWithChildren {
+  form: UseFormReturn;
+  onSubmit?: (values: any) => void;
+}
+
+const Form: FC<FormProps> & InternalForm = ({ children, form, onSubmit }) => {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (onSubmit) {
+      onSubmit({});
+    }
+  };
+
+  return (
+    <FormContext.Provider value={form}>
+      <form onSubmit={handleSubmit}>{children}</form>
+    </FormContext.Provider>
+  );
 };
 
 export default Form;
 
-export const useForm = () => {};
+interface ItemProps extends PropsWithChildren {
+  label: string;
+}
+
+const Item: FC<ItemProps> = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+interface InputProps {
+  as?: ReactElement;
+  fieldKey: string;
+}
+
+const Input: FC<InputProps> = ({ as, fieldKey }) => {
+  const { values, handleValue } = useContext(FormContext);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    handleValue(fieldKey, e.target.value);
+  };
+
+  if (as) {
+    return cloneElement(as, {
+      value: String(values.name),
+      onChange: handleChange,
+    });
+  }
+
+  return <input value={String(values[fieldKey])} onChange={handleChange} />;
+};
+
+Form.Item = Item;
+Form.Input = Input;
