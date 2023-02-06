beb6d9232c098ba175fee00d2c2ef7f262c94371
dnlwllms <daniel.williams.kr@gmail.com>
2023-02-06 11:05:47 +0900

    add: Dialog Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import Form, { useForm, regExpExample } from "./packages/form";
+import Dialog from "./packages/dialog";
 import { NumberUtility } from "./packages/util";
 
 const { getTelNumber } = NumberUtility;
@@ -49,6 +50,8 @@ function App() {
     form.handleValue("tel", getTelNumber(e.target.value));
   };
 
+  const [openKey, setOpenKey] = useState(0);
+
   return (
     <div className="App">
       <Form form={form} onSubmit={console.log}>
@@ -70,6 +73,27 @@ function App() {
         </Form.Item>
         <button>submit</button>
       </Form>
+      {[1, 2].map((key) => {
+        return (
+          <Dialog
+            key={key}
+            open={openKey === key}
+            onClose={() => setOpenKey(0)}
+          >
+            <Dialog.Trigger>
+              <button onClick={() => setOpenKey(key)}>open{key}</button>
+            </Dialog.Trigger>
+            <Dialog.Body>
+              {(rect) => {
+                console.log(rect);
+                return (
+                  <div onClick={(e) => e.stopPropagation()}>하이{key}</div>
+                );
+              }}
+            </Dialog.Body>
+          </Dialog>
+        );
+      })}
     </div>
   );
 }
--- src/packages/dialog/index.tsx
+++ src/packages/dialog/index.tsx
@@ -1,13 +1,114 @@
-import { createContext, FC, PropsWithChildren } from "react";
+import {
+  cloneElement,
+  createContext,
+  FC,
+  PropsWithChildren,
+  ReactElement,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { createPortal } from "react-dom";
 
-const dialogContextDefaultValue = {};
+type InternalDialog = {
+  Trigger: FC<TriggerProps>;
+  Body: FC<BodyProps>;
+};
+
+const dialogContextDefaultValue = {
+  isOpen: false,
+  triggerRect: new DOMRect(),
+  handleOpen: console.debug,
+  handleClose: console.debug,
+};
 
 const DialogContext = createContext(dialogContextDefaultValue);
 
-interface DialogProps extends PropsWithChildren {}
+interface DialogProps extends PropsWithChildren {
+  open?: boolean;
+  onClose?: () => void;
+}
 
-const Dialog: FC<DialogProps> = ({ children }) => {
-  return <DialogContext.Provider value={{}}>{children}</DialogContext.Provider>;
+const Dialog: FC<DialogProps> & InternalDialog = ({
+  children,
+  open,
+  onClose,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [triggerRect, setTriggerRect] = useState(new DOMRect());
+
+  const handleClose = useCallback(
+    (e: MouseEvent) => {
+      setIsOpen(false);
+
+      if (onClose) {
+        onClose();
+      }
+    },
+    [onClose]
+  );
+
+  useEffect(() => {
+    window.addEventListener("click", handleClose);
+    return () => {
+      window.removeEventListener("click", handleClose);
+    };
+  }, [handleClose]);
+
+  const handleOpen = (e: MouseEvent) => {
+    const target = e.target as HTMLElement;
+
+    if (target) {
+      setTriggerRect(target.getBoundingClientRect());
+    }
+
+    setIsOpen(!isOpen);
+  };
+  return (
+    <DialogContext.Provider
+      value={{
+        isOpen: open === undefined ? isOpen : open,
+        triggerRect,
+        handleOpen,
+        handleClose,
+      }}
+    >
+      {children}
+    </DialogContext.Provider>
+  );
 };
+
+interface TriggerProps {
+  children: ReactElement;
+}
+
+const Trigger: FC<TriggerProps> = ({ children }) => {
+  const { handleOpen } = useContext(DialogContext);
+
+  return cloneElement(children, {
+    onClick: (e: MouseEvent) => {
+      e.stopPropagation();
+      handleOpen(e);
+
+      if (children.props.onClick) {
+        children.props.onClick();
+      }
+    },
+  });
+};
+
+interface BodyProps {
+  children: (rect: DOMRect) => ReactElement;
+}
+
+const Body: FC<BodyProps> = ({ children }) => {
+  const { isOpen, triggerRect } = useContext(DialogContext);
+
+  return isOpen ? createPortal(children(triggerRect), document.body) : null;
+};
+
+Dialog.Trigger = Trigger;
+Dialog.Body = Body;
 
 export default Dialog;
