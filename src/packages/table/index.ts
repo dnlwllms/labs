e4e77fc6bfe233860251428c4c7f2a307526f48f
dnlwllms <daniel.williams.kr@gmail.com>
2023-02-08 15:25:28 +0900

    fix: Filter export

--- src/packages/table/index.ts
+++ src/packages/table/index.ts
@@ -1,6 +1,7 @@
 import Table from "./Table";
+import Filter from "./Filter";
 import { useColumnResize } from "./useColumnResize";
 
 export default Table;
 
-export { useColumnResize };
+export { useColumnResize, Filter };
