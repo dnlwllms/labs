10201cb3986e51bfce9af9e31a7a0292216ec7eb
dnlwllms <daniel.williams.kr@gmail.com>
2023-02-10 14:59:57 +0900

    feat: FilterUtility - Add filter util function

--- src/packages/util/index.ts
+++ src/packages/util/index.ts
@@ -4,6 +4,7 @@ import { ObjectUtility } from "./ObjectUtility";
 import { DateUtility } from "./DateUtility";
 import { TestUtility } from "./TestUtility";
 import { WindowUtility } from "./WindowUtility";
+import { FilterUtility } from "./FilterUtility";
 import { UtilError } from "./UtilError";
 
 export * from "./types";
@@ -15,5 +16,6 @@ export {
   DateUtility,
   TestUtility,
   WindowUtility,
+  FilterUtility,
   UtilError,
 };
