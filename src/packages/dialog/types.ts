417bfbd0e3eeee7f342d0fa0a5b0fff6986ea5a1
dnlwllms <daniel.williams.kr@gmail.com>
2023-02-13 08:55:52 +0900

    fix: dialog package - add annotation - name of dialog body popup interface

--- src/packages/dialog/types.ts
+++ src/packages/dialog/types.ts
@@ -1,35 +1,39 @@
 import { FC, PropsWithChildren, ReactElement } from "react";
 import { MovePositionOption } from "@dnlwllms/util";
 
+// Dialog 내장 컴포넌트
 export type InternalDialog = {
   Trigger: FC<DialogTriggerProps>;
   Body: DialogBodyComponent;
 };
 
+// Dialog.Body 내장 컴포넌트
 export type InternalDialogBody = {
-  Popup: FC<PopupProps>;
+  Popup: FC<DialogBodyPopupProps>;
 };
 
+// Dialog 컴포넌트와 props 인터페이스
+export interface DialogProps extends PropsWithChildren {}
 export interface DialogComponent extends FC<DialogProps>, InternalDialog {}
 
-export interface DialogProps extends PropsWithChildren {}
-
+// Dialog.Trigger props 인터페이스
 export interface DialogTriggerProps {
   children: ReactElement;
 }
 
+// Dialog.Body 컴포넌트와 props 인터페이스
 export interface DialogBodyProps {
   children: (renderProps: {
     triggerRect?: DOMRect;
     handleClose: () => void;
   }) => ReactElement;
 }
-
 export interface DialogBodyComponent
   extends FC<DialogBodyProps>,
     InternalDialogBody {}
 
-export interface PopupProps {
+// Dialog.Body.Popup props 인터페이스
+export interface DialogBodyPopupProps {
   children: ReactElement;
   triggerRect?: DOMRect;
   positionOption?: MovePositionOption;
